23ca295325b308c6575caa28fe272226b47688e6
jazzyfact95@gmail.com <jazzyfact95@gmail.com>
2021-10-26 00:54:34 +0900

    master

--- practice.js
+++ practice.js
@@ -3,12 +3,15 @@
 var a = 1;
 
 function outer(){
-    console.log(a);
+    console.log(a);     //첫번 째 로그 출력
     function inner(){
-        console.log(a);
+        console.log(a); //두번 째 로그 출력
         var a = 3;
     }
-    var a = 2;
+    inner();
+
+    console.log(a);     // 세번 째 로그 출력
 }
 
 outer();
+console.log(a);         //네번 째 로그 출력 후 전역 콘텍스트 종료
